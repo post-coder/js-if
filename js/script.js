5493141be8f7059c3821a02926e43b3e37bc9df0
post-coder <gabrielspanu.dev@gmail.com>
2024-01-26 12:33:51 +0100

    pari e dispari: versione non ottimizzata

--- js/script.js
+++ js/script.js
@@ -1,28 +1,70 @@
 
-// creiamo un OGGETTO 
+// numeri a caso
 
-const myTime = new Date();
-// questo oggetto myTime contiene tutte le informazioni temporali
-//  dell'istante in cui è stata eseguita quella determinata riga di codice
+// Math.random() crea un numero casuale da 0 a 1, 0 è incluso, 1 è escluso
 
-/*
+// possiamo però moltiplicare il numero a nostro piacimento per gestirne il range
 
-quanto manca: utilizzando le funzioni dell'oggetto Date,
- stabilire l'ora attuale e attraverso una operazione matematica 
- stabilire i minuti mancanti allo scoccare della prossima ora.
-*/
+// l'utente prima sceglie se giocare per pari o dispari
+// inserisce un numero casuale da 1 a 5
+// il computer genera un numero da 1 a 5 casuale
+// controlliamo se l'utente ha vinto o perso
 
-// "sono le xx:yy, mancano  tot minuti alle 12"
 
-const currentHour = myTime.getHours();
-const currentMinutes = myTime.getMinutes();
+const userChoice = prompt("pari o dispari?")
+// test
+// const userChoice = "dispari";
 
-const minutesLeft = 60 - currentMinutes;
+// test
+const userNumber = Number(prompt("scegli un numero da 1 a 5"));
+// const userNumber = 3;
 
-// console.log("minuti mancanti", minutesLeft);
 
-let timing = `Sono le ${currentHour}:${currentMinutes}, mancano ${minutesLeft} minuti alle ${currentHour + 1}:00`;
+const computerNumber = Math.floor( Math.random() * 5 + 1);
 
-// console.log(timing);
+console.log("numero utente: ", userNumber);
+console.log("numero computer: ", computerNumber);
 
-document.getElementById("esito").innerHTML = timing;
+
+// utilizzando un nuovo operatore: modulo
+
+// console.log(16 % 2); // in questo caso ci da resto 0
+// console.log(15 % 2); // in questo caso ci da resto 1
+// il modulo ci da il resto della divisione algebrica tra i due numeri
+
+
+const sum = userNumber + computerNumber;
+
+console.log("sum",sum);
+
+
+if (sum % 2 == 0) {
+
+    // somma è pari
+
+    document.getElementById('esito').innerHTML = "Pari";
+
+    if (userChoice == "pari") {
+        document.getElementById('vittoria').innerHTML = "Utente ha vinto!"
+    } else {
+        document.getElementById('vittoria').innerHTML = "Utente ha perso!"
+    }
+
+} else {
+
+    // la somma è dispari
+    document.getElementById('esito').innerHTML = "Dispari";
+
+
+    if(userChoice == "dispari") {
+
+        document.getElementById('vittoria').innerHTML = "Utente ha vinto!"
+
+    } else {
+
+        document.getElementById('vittoria').innerHTML = "Utente ha perso!"
+
+    }
+
+
+}
