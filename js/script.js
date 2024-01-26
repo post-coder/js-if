a1e3bf6f5108c6fb9e6a87aa5009d3d75e369b85
post-coder <gabrielspanu.dev@gmail.com>
2024-01-26 11:57:49 +0100

    lavorare col tempo

--- js/script.js
+++ js/script.js
@@ -1,54 +1,28 @@
-// controllare quale delle due parole dell'utente sia la più lunga
-// (quella che abbia più lettere)
 
+// creiamo un OGGETTO 
 
-const word1 = prompt("inserisci la prima parola");
-const word2 = prompt("inserisci la seconda parola");
+const myTime = new Date();
+// questo oggetto myTime contiene tutte le informazioni temporali
+//  dell'istante in cui è stata eseguita quella determinata riga di codice
 
-// test
-// const word1 = "pellicano";
-// const word2 = "cane";
+/*
 
-// controllo i valori delle proprietà "length" di ogni stringa
-console.log(word1.length);
-console.log(word2.length);
+quanto manca: utilizzando le funzioni dell'oggetto Date,
+ stabilire l'ora attuale e attraverso una operazione matematica 
+ stabilire i minuti mancanti allo scoccare della prossima ora.
+*/
 
-// controlliamo quale sia la parola più lunga?
+// "sono le xx:yy, mancano  tot minuti alle 12"
 
+const currentHour = myTime.getHours();
+const currentMinutes = myTime.getMinutes();
 
-if (word1.length > word2.length) {
-    
-    
-    const difference =  word1.length - word2.length;
+const minutesLeft = 60 - currentMinutes;
 
+// console.log("minuti mancanti", minutesLeft);
 
-    if (difference === 1) {
+let timing = `Sono le ${currentHour}:${currentMinutes}, mancano ${minutesLeft} minuti alle ${currentHour + 1}:00`;
 
-        document.getElementById("esito").innerHTML = `
-            La prima parola "${word1}" è più lunga della seconda parola "${word2}"
-            di ${difference} carattere.
-        `;
+// console.log(timing);
 
-    } else {
-
-        document.getElementById("esito").innerHTML = `
-            La prima parola "${word1}" è più lunga della seconda parola "${word2}"
-            di ${difference} caratteri.
-        `;
-        
-    }
-
-
-    
-
-} else if (word1.length < word2.length)  {
-
-    const difference =  word2.length - word1.length;
-
-
-    document.getElementById("esito").innerHTML = `La prima parola "${word1}" è più corta della seconda parola "${word2}" di ${difference} caratteri`;
-
-
-} else {
-    document.getElementById("esito").innerHTML = "Le parole sono di lunghezza uguale";
-}
+document.getElementById("esito").innerHTML = timing;
