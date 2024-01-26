6a6199c63dce4dec8c96a376e607e4495ee94add
post-coder <gabrielspanu.dev@gmail.com>
2024-01-26 11:37:32 +0100

    esempi lunghezza parole

--- js/script.js
+++ js/script.js
@@ -1,22 +1,54 @@
-// chiediamo a due utenti la loro età
-// controlliamo chi sia il più grande
-
-const age1 = 27;
-const age2 = 22;
+// controllare quale delle due parole dell'utente sia la più lunga
+// (quella che abbia più lettere)
 
 
-if (age1 > age2) {
+const word1 = prompt("inserisci la prima parola");
+const word2 = prompt("inserisci la seconda parola");
 
-    document.getElementById("esito").innerHTML = "L'utente 1 è più grande dell'utente 2";
+// test
+// const word1 = "pellicano";
+// const word2 = "cane";
 
-} else if (age1 < age2) {
+// controllo i valori delle proprietà "length" di ogni stringa
+console.log(word1.length);
+console.log(word2.length);
 
-    document.getElementById("esito").innerHTML = "L'utente 1 è più piccolo dell'utente 2";
+// controlliamo quale sia la parola più lunga?
+
+
+if (word1.length > word2.length) {
+    
+    
+    const difference =  word1.length - word2.length;
+
+
+    if (difference === 1) {
+
+        document.getElementById("esito").innerHTML = `
+            La prima parola "${word1}" è più lunga della seconda parola "${word2}"
+            di ${difference} carattere.
+        `;
+
+    } else {
+
+        document.getElementById("esito").innerHTML = `
+            La prima parola "${word1}" è più lunga della seconda parola "${word2}"
+            di ${difference} caratteri.
+        `;
+        
+    }
+
+
+    
+
+} else if (word1.length < word2.length)  {
+
+    const difference =  word2.length - word1.length;
+
+
+    document.getElementById("esito").innerHTML = `La prima parola "${word1}" è più corta della seconda parola "${word2}" di ${difference} caratteri`;
+
 
 } else {
-
-    document.getElementById("esito").innerHTML = "L'utente 1 ha la stessa età dell'utente 2";
-
+    document.getElementById("esito").innerHTML = "Le parole sono di lunghezza uguale";
 }
-
-console.log("finito")
